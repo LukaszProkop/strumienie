c16b34ba4206031c7ebe8ba8286b7b800e2e3d18
Lukasz Prokop <wuzu222@gmail.com>
2021-03-04 04:23:14 +0100

    Poprawa enkapsulacji oraz opisu zadań

--- src/main/java/com/github/lukaszprokop/TextAnalyzer.java
+++ src/main/java/com/github/lukaszprokop/TextAnalyzer.java
@@ -2,40 +2,39 @@ package com.github.lukaszprokop;
 
 import java.util.Map;
 
+/* TODO: Dodaj swoja implementację metod korzystając ze Strumieni oraz wykonaj przygotowane testy.
+    Nie zmieniaj synatury metod.*/
+
 class TextAnalyzer {
 
     public static void main(String[] args) {
 
         String path = "src/main/resources/text.txt";
 
-        System.out.printf("\nNajdluzszy wyraz: %s\n", longestWord(path));
+        System.out.printf("\nNajdluzszy wyraz: %s%n", longestWord(path));
 
-        System.out.printf("\nSrednia ilosc liter w wyrazach: %.1f\n", averageLetterNumberInWords(path));
+        System.out.printf("\nSrednia ilosc liter w wyrazach: %.1f%n", averageLetterNumberInWords(path));
 
-        System.out.printf("\nIlosc wyrazow w pliku: %d\n", countWords(path));
+        System.out.printf("\nIlosc wyrazow w pliku: %d%n", countWords(path));
 
         System.out.println("\nWystąpienia liter:");
         countCharacters(path).forEach((character, integer) -> System.out.printf("%c: %d\n", character, integer));
 
     }
 
-    public static String longestWord(String inputPath) {
-        /* TODO: Dodaj swoja implementację korzystając ze Strumieni i wykonaj przygotowane testy.
-            Nie zmieniaj synatury metody.*/
+    static String longestWord(String inputPath) {
+        /* TODO: Twoja implementacja*/
     }
 
-    public static double averageLetterNumberInWords(String inputPath) {
-        /* TODO: Dodaj swoja implementację korzystając ze Strumieni i wykonaj przygotowane testy.
-            Nie zmieniaj synatury metody oraz testów.*/
+    static double averageLetterNumberInWords(String inputPath) {
+        /* TODO: Twoja implementacja*/
     }
 
-    public static long countWords(String inputPath) {
-        /* TODO: Dodaj swoja implementację korzystając ze Strumieni i wykonaj przygotowane testy.
-            Nie zmieniaj synatury oraz testów.*/
+    static long countWords(String inputPath) {
+        /* TODO: Twoja implementacja*/
     }
 
-    public static Map<Character, Integer> countCharacters(String inputPath) {
-        /* TODO: Dodaj swoja implementację korzystając ze Strumieni i wykonaj przygotowane testy.
-            Nie zmieniaj synatury oraz testów.*/
+    static Map<Character, Integer> countCharacters(String inputPath) {
+        /* TODO: Twoja implementacja*/
     }
 }
